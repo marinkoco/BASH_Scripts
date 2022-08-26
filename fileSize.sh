6942706c09a60c5d937fdb0b73a327c37df7182e
marinkoco <69796741+marinkoco@users.noreply.github.com>
2022-08-26 11:43:03 +0200

    Add files via upload

---
+++ fileSize.sh
@@ -0,0 +1,10 @@
+#this script is file size and writting it to a log
+#recurrence to be handled with cron job
+
+!/bin/bash
+
+#if needed to be set in a time stamp log format uncomment lines 7 and 8 and comment line 10
+#current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+#echo -n "[INFO]" ; echo -n $current_time ; echo -n ": " ; du -sh <path to the file>
+
+du -sh <path to the file> >> <path to the log>
